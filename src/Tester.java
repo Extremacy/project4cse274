c0c59fe20b4e76b64c35b14c705ff252ec903555
PJ Duimstra <duimstpj@miamioh.edu>
2021-12-01 20:25:52 -0500

    Initial commit

---
+++ src/Tester.java
@@ -0,0 +1,11 @@
+public class Tester {
+
+    public static void main(String[] args) {
+        Graph.returnAddress = true;
+        Graph.useDistCost = false;
+
+        Graph g = new Graph("MapInformation.txt");
+        System.out.println(g);
+    }
+
+}
